0edf25e4082fea7f5390d72e374b0e1fe30aa544
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 00:56:53 -0500

    Auto-commit: Actualización de código

---
+++ string arrays y las tablas hash/array/arrays_multidimensionales.py
@@ -0,0 +1,10 @@
+# archivo: arrays_multidimensionales.py
+import numpy as np
+
+# Crear una matriz 2x3
+matriz = np.array([[1, 2, 3], [4, 5, 6]])
+print(matriz)
+
+# Acceder a un elemento específico
+elemento = matriz[1, 2]  # Segundo fila, tercera columna
+print(elemento)
