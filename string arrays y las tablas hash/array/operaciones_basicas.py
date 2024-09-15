85fc98b3b16a032b92eac1d6bf40fa652d759477
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 00:56:23 -0500

    Auto-commit: Actualización de código

---
+++ string arrays y las tablas hash/array/operaciones_basicas.py
@@ -0,0 +1,14 @@
+# archivo: operaciones_basicas.py
+import numpy as np
+
+# Crear dos arrays
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# Suma de arrays
+suma = a + b
+print(suma)
+
+# Multiplicación por un escalar
+multiplicacion = a * 2
+print(multiplicacion)
