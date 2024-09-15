9c7fbc42f621b25028822c1429b781b3195bd86b
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 00:55:50 -0500

    Auto-commit: Actualización de código

---
+++ string arrays y las tablas hash/array/arrays_basicos.py
@@ -0,0 +1,10 @@
+# archivo: arrays_basicos.py
+import numpy as np
+
+# Crear un array de 5 elementos
+mi_array = np.array([1, 2, 3, 4, 5])
+print(mi_array)
+
+# Acceder al segundo elemento
+segundo_elemento = mi_array[1]
+print(segundo_elemento)
